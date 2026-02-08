efa45902ff22dc5f0b0761f312fe8a6c5075f237
LieeAutoDL <2497791272@qq.com>
2026-02-09 00:45:39 +0800

    run cross-area vit-base baseline

--- Game4Loc/train.sh
+++ Game4Loc/train.sh
@@ -22,3 +22,11 @@ python train_visloc.py --data_root <The directory of the UAV-VisLoc dataset> --t
 python train_gta.py --data_root "game4loc\dataset\GTA-UAV-LR\GTA-UAV-LR-baidu" --train_pairs_meta_file "mini-cross-area-drone2sate-train.json" --test_pairs_meta_file "mini-cross-area-drone2sate-test.json" --gpu_ids 0 --epoch 10 --model "vit_medium_patch16_rope_reg1_gap_256.sbb_in1k" --lr 0.0001 --batch_size 8
 Recall@1: 39.8048 - Recall@5: 64.7505 - Recall@10: 72.7766 - Recall@top1: 92.4078 - AP: 51.3155 - SDM@1: 0.7071 - SDM@3: 0.6384 - SDM@5: 0.5978 - Dis@1: 684.6771 - Dis@3: 901.5898 - Dis@5: 1015.6826
 ssh -p 40213 root@connect.westc.gpuhub.com
+
+
+# Cross-area setting with weighted-InfoNCE k=5
+python train_gta.py --data_root "\root\autodl-tmp\dataset\GTA-UAV-LR\GTA-UAV-LR-baidu" --train_pairs_meta_file "cross-area-drone2sate-train.json" --test_pairs_meta_file "cross-area-drone2sate-test.json" --gpu_ids 0 --with_weight --k 5 --epoch 5 --model 'vit_base_patch16_rope_reg1_gap_256.sbb_in1k' --lr 0.0001 --batch_size 32
+------------------------------[Evaluate]------------------------------
+Extract Features and Compute Scores:
+Processing each query: 100%|███████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████| 9056/9056 [00:19<00:00, 454.19it/s]
+Recall@1: 61.2853 - Recall@5: 85.3578 - Recall@10: 89.2005 - Recall@top1: 96.9302 - AP: 71.6573 - SDM@1: 0.8215 - SDM@3: 0.7559 - SDM@5: 0.7088 - Dis@1: 333.0190 - Dis@3: 527.0131 - Dis@5: 662.3857
